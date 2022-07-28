3c6ebf6bdfbc23bca2f471bf2edd4c2936537709
Simon Alford <sca63@cornell.edu>
2022-07-28 16:35:57 -0400

    test gpg

--- run2.sh
+++ run2.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
+# sbatch --partition ellis run.sh --batch_size 128 --neurosym --n 20000
+# sbatch --partition ellis run.sh --batch_size 128 --neurosym --n 20000
 
-# sbatch --partition ellis run.sh --batch_size 128 --neurosym --n 20000
-# sbatch --partition ellis run.sh --batch_size 128 --neurosym --n 20000
 
 sbatch run.sh --model hmm-homo --abstract_pen 1 --lr 8E-4 --seed 1
 sbatch run.sh --model hmm-homo --abstract_pen 1 --lr 8E-4 --seed 2
